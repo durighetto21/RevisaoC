bc762f7db76d4f9c24af83b3511f50ed07ca777f
Douglas <durighetto21@gmail.com>
2020-08-22 22:59:35 -0300

    Criação do Repositóriio e Inserção no Git

---
+++ RevisaoVStudio/Classes/Produto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RevisaoVStudio.Classes
+{
+    class Produto
+    {
+        private List<Produto> produtos;
+        public string Nome { get; set; }
+        public string Categoria { get; set; }
+
+        public double Valor { get; set; }
+        public List<int> CustoMedio;
+        public Produto()
+        {
+            produtos = new List<Produto>();
+        }
+
+        public List<Produto> Listar()
+        {
+            produtos.Add(new Produto() { Nome = "Maça", Categoria = "Fruta", Valor = 10, CustoMedio = new List<int>{ 7, 8, 3, 10 } });
+            produtos.Add(new Produto() { Nome = "Pera", Categoria = "Fruta", Valor = 10, CustoMedio = new List<int> { 1, 2, 3, 4 } });
+            produtos.Add(new Produto() { Nome = "Caju", Categoria = "Fruta", Valor = 10, CustoMedio = new List<int> { 1, 2, 3, 4 } });
+            produtos.Add(new Produto() { Nome = "Banana", Categoria = "Fruta", Valor = 10, CustoMedio = new List<int> { 1, 2, 3, 4 } });
+            produtos.Add(new Produto() { Nome = "Melância", Categoria = "Fruta", Valor = 10, CustoMedio = new List<int> { 1, 2, 3, 4 } });
+            produtos.Add(new Produto() { Nome = "Patinho", Categoria = "Carne", Valor = 10, CustoMedio = new List<int> { 1, 2, 3, 4 } });
+            produtos.Add(new Produto() { Nome = "Alcatra", Categoria = "Carne", Valor = 10, CustoMedio = new List<int> { 1, 2, 3, 4 } });
+            produtos.Add(new Produto() { Nome = "Picanha", Categoria = "Carne", Valor = 10, CustoMedio = new List<int> { 1, 2, 3, 4 } });
+            produtos.Add(new Produto() { Nome = "Fraldinha", Categoria = "Carne", Valor = 10, CustoMedio = new List<int> { 1, 2, 3, 4 } });
+            produtos.Add(new Produto() { Nome = "Acem", Categoria = "Carne", Valor = 10, CustoMedio = new List<int> { 1, 2, 3, 4 } });
+            produtos.Add(new Produto() { Nome = "Arroz", Categoria = "Alimento", Valor = 5, CustoMedio = new List<int> { 1, 2, 3, 4 } });
+
+            return produtos;
+        }
+    }
+}
---
+++ RevisaoVStudio/Program.cs
@@ -0,0 +1,111 @@
+﻿using RevisaoVStudio.Classes;
+using System;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace RevisaoVStudio
+{
+    class Program
+    {
+        protected static int GetPercentile(Produto s)
+        {
+            double avg = s.CustoMedio.Average();
+            return avg > 0 ? (int)avg : 0;
+        }
+        static void Main(string[] args)
+        {
+            Produto produtosObj = new Produto();
+
+            var produtos = produtosObj.Listar();
+
+            #region Exemplos GroupBy
+
+            #region GroupBy Simples
+            //var ProdCat = from prod in produtos
+            //              group prod by prod.Categoria into GrupoCat
+            //              select GrupoCat;
+
+            //foreach  (var cat in ProdCat)
+            //{
+            //    Console.WriteLine($"Categoria: {cat.Key}");
+            //    foreach (var item in cat)
+            //    {
+            //        Console.WriteLine($" Produto: {item.Nome}");
+            //    }
+            //} 
+            #endregion
+
+            #region Exemplo GroupBy let e atributo anônimo
+            //var queryNumericRange = from produto in produtos
+            //                        let percentile = GetPercentile(produto)
+            //                        group new { produto.Nome, produto.Valor } by percentile into percentGroup
+            //                        orderby percentGroup.Key
+            //                        select percentGroup;
+
+            //foreach (var grupoProduto in queryNumericRange)
+            //{
+            //    Console.WriteLine($"Key: {grupoProduto.Key}");
+            //    foreach (var item in grupoProduto.OrderBy(x => x.Nome))
+            //    {
+            //        Console.WriteLine($"\t{item.Nome}, {item.Valor}");
+            //    }
+            //}
+            #endregion
+
+            #region Exemplo tipo anônimo na chave do GroupBy
+
+            //var queryProdutos = from prod in produtos
+            //                    group prod by new { Nome = prod.Nome[0], Valor = prod.Valor > 5, Categoria = prod.Categoria } into GroupProd
+            //                    orderby GroupProd.Key.Nome
+            //                    orderby GroupProd.Key.Categoria
+            //                    select GroupProd;
+
+            //foreach (var grupo in queryProdutos)
+            //{
+            //    string s = grupo.Key.Valor == true ? "Maior do que" : "Menor do que ";
+
+            //    Console.WriteLine($"Produtos começados com a letra {grupo.Key.Nome} com valor {s} 5: ");
+            //    foreach (var item in grupo)
+            //    {
+            //        Console.WriteLine($"\t{item.Nome}");
+            //    }
+
+            //}
+            #endregion
+
+            #endregion
+
+
+            #region Exemplo Consulta Aninhada
+            //Separar produtos por categoria e agrupar por letra inicial, ordenando os dados por ordem alfabética.
+
+            //var queryCat = from produto in produtos
+            //               group produto by produto.Categoria into Grupo1
+            //               from Grupo2 in
+            //                   (from prod in Grupo1
+            //                    group prod by prod.Nome[0]
+            //                    ).OrderBy(x => x.Key)
+            //               group Grupo2 by Grupo1.Key;
+
+            //foreach (var cat in queryCat.OrderBy(x => x.Key))
+            //{
+            //    Console.WriteLine($"Categoria: { cat.Key}");
+            //    foreach (var letra in cat)
+            //    {
+            //        Console.WriteLine($"\tProdutos começados com a letra {letra.Key}:");
+            //        foreach (var prod in letra)
+            //        {
+            //            Console.WriteLine($"\t\t {prod.Nome} , R${prod.Valor}");
+            //        }
+
+            //    }
+
+            //} 
+            #endregion
+
+
+            Console.ReadKey();
+        }
+    }
+}
